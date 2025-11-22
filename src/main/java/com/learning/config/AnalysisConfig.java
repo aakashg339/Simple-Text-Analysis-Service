9f6fe18b1f6299f7bbf3a07eb755a5ddb9737ac7
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2025-11-22 18:05:42 +0530

    Implementation-logic Made use of @Value in Config class

--- src/main/java/com/learning/config/AnalysisConfig.java
+++ src/main/java/com/learning/config/AnalysisConfig.java
@@ -1,10 +1,19 @@
 package com.learning.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.learning")
 public class AnalysisConfig {
+
+    private String testProperty;
+
+    public AnalysisConfig(@Value("aDefaultValue") String testProperty) {
+        this.testProperty = testProperty;
+
+        System.out.println("Injected testProperty value: " + this.testProperty);
+    }
 
 }
